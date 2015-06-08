51ee282778183966737e42adcbacdad34038236d
Ilya Yakubovich <ilya.yakubovich@gmail.com>
2015-06-07 21:12:14 -0400

    Added some recent work and rewrote some sections

--- js/main.js
+++ js/main.js
@@ -17,26 +17,55 @@ $(document).ready(function() {
     $callout.css("background-position", offset + "px 0");
   }
 
-  $(".work-nav a").click(function() {
-    $selected = $(this);
-    $(".work-nav a").removeClass("selected");
-    $selected.addClass("selected");
-    resize();
-    var $slide = $("#carousel-" + $(this).attr("href").split("#")[1]);
-    if ($("body").attr("id") == "index") {
-      $(".carousel-item").css("opacity", 0).removeClass("selected");
-      $slide.css("opacity", 1);
-    } else {
-      $(".about").hide().removeClass("selected");
-      $slide.show();
-    }
-    $slide.addClass("selected");
+  var $carousel = $("#carousel");
+  var $cloned = $carousel.clone().removeClass("main-carousel").attr("name", "cloned");
+  $cloned.appendTo($("body"));
+
+  $(".next").click(function() {
+    $(".work-nav .selected").parent().next().find("a").click()
   });
 
-  $("a[href=" + location.hash + "]").click();
+  $(".work-nav a").click(function() {
+
+    if ($(this).parent().parent().parent().parent().attr("name") === "cloned")
+      var $other = $carousel;
+    else
+      var $other = $cloned;
+
+    $selected = $(this);
+
+    $(".work-nav a").removeClass("selected");
+    $selected.addClass("selected");
+    $other.find("a[href=" + $(this).attr("href") + "]").addClass("selected");
+
+    resize();
+    var project = $(this).attr("href").split("#")[1];
+    var $slide = $("#carousel-" + project);
+
+    $carousel.removeClass().addClass("bg-" + project);
+    $cloned.removeClass().addClass("bg-" + project);
+
+    $(".about").hide().removeClass("selected");
+    $slide.show();
+    $slide.addClass("selected");
+
+	  $("body").animate({scrollTop: 0}, 500);
+  });
+
+  $("#carousel[name='main-carousel'] a[href=" + location.hash + "]").click();
 
   $("#index .carousel-item").click(function() {
     window.location.href = "work.html" + $selected[0].hash;
+  });
+
+  var carouselPosition = $carousel.offset().top;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > carouselPosition) {
+      $cloned.show();
+    } else {
+      $cloned.hide();
+    }
   });
 
 });
